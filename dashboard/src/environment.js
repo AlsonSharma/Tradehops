1c4f6fef1558ae205ffa6dc6c6d21ec8334c34b0
Student <sharmaalson@gmail.com>
2025-03-24 13:15:37 -0400

    deploying frontend in render

--- dashboard/src/environment.js
+++ dashboard/src/environment.js
@@ -6,7 +6,7 @@ const server = IS_PROD
 
 
   const frontend = IS_PROD ? 
- "https://tourmaline-pie-352391.netlify.app"
+ "https://tradehops-1.onrender.com"
   : "http://localhost:5173";
 
 export {server, frontend};
